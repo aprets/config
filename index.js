116ea48b942b5b6e9d98a6f183c529ea2cc09ab2
aprets <15954684+aprets@users.noreply.github.com>
2023-03-25 19:14:45 +0000

    v2

--- index.js
+++ index.js
@@ -3,46 +3,41 @@ module.exports = {
   env: {
     browser: true,
     node: true,
+    jest: true,
   },
-  plugins: ["import", "react", "jsx-a11y"],
-  extends: [
-    "airbnb",
-    "airbnb/hooks",
-    "airbnb-typescript",
-    "plugin:@typescript-eslint/recommended",
-  ],
+  plugins: ['import', 'react', 'jsx-a11y', '@typescript-eslint', 'prettier'],
+  extends: ['airbnb', 'airbnb/hooks', 'airbnb-typescript', 'plugin:@typescript-eslint/recommended'],
   rules: {
-    "max-len": [
-      "error",
+    'prettier/prettier': 'error',
+    'max-len': [
+      'error',
       {
         code: 160,
         ignoreStrings: true,
       },
     ],
-    "func-style": ["error", "declaration", { allowArrowFunctions: true }],
-    "import/no-cycle": 1,
+    // prevent const x = function() {}
+    'func-style': ['error', 'declaration', { allowArrowFunctions: true }],
     // naming export can be beneficial for debugging, readability etc.
-    "import/prefer-default-export": 0,
+    'import/prefer-default-export': 'off',
     // mirror the airbnb config, but remove for..in and for..of
     // as they are reasonable in node or modern browsers
-    "no-restricted-syntax": [
-      "error",
+    'no-restricted-syntax': [
+      'error',
       {
-        selector: "LabeledStatement",
-        message:
-          "Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.",
+        selector: 'LabeledStatement',
+        message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
       },
       {
-        selector: "WithStatement",
-        message:
-          "`with` is disallowed in strict mode because it makes code impossible to predict and optimize.",
+        selector: 'WithStatement',
+        message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
       },
     ],
     // mirror the airbnb config, but allow some exceptions
-    "no-underscore-dangle": [
-      "error",
+    'no-underscore-dangle': [
+      'error',
       {
-        allow: ["_id"],
+        allow: ['_id'],
         allowAfterThis: false,
         allowAfterSuper: false,
         enforceInMethodNames: true,
@@ -50,31 +45,57 @@ module.exports = {
     ],
 
     /* React-specific rules */
-    "react/react-in-jsx-scope": 0,
-    "react/jsx-indent-props": 0,
-    "react/no-danger": 0,
-    // Consider enabling those in the future
-    //'react/jsx-props-no-spreading': 0,
-    //'react/require-default-props': 0,
+    // Not needed with React 17+
+    'react/react-in-jsx-scope': 'off',
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    // consider fully disabling if exceptions pop up too much
+    'react/jsx-props-no-spreading': [
+      'error',
+      {
+        html: 'enforce',
+        custom: 'enforce',
+        explicitSpread: 'ignore',
+        exceptions: ['FormCheckbox', 'FormInput', 'FormRadio', 'FormSelect', 'FormTextArea', 'FormToggle', 'TextField'],
+      },
+    ],
+    // Consider uncommenting those in the future
+    // can be excessive as react already kind of does it
+    // 'react/no-danger': 'off',
+    // 'react/require-default-props': 'off',
 
     /* TypeScript-specific rules */
-    "@typescript-eslint/member-delimiter-style": [
-      "error",
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
       {
         multiline: {
-          delimiter: "comma",
+          delimiter: 'comma',
           requireLast: true,
         },
       },
     ],
-    // Consider enabling those in the future
-    //'@typescript-eslint/explicit-module-boundary-types': 0,
+    // Consider uncommenting those in the future
+    // '@typescript-eslint/explicit-module-boundary-types': 'off',
+
+    /* Prettier compatibility rules */
+    'implicit-arrow-linebreak': 'off',
   },
   overrides: [
     {
-      files: ["**/*.tsx"],
+      files: ['**/*.tsx'],
       rules: {
-        "react/prop-types": 0,
+        'react/prop-types': 'off',
+      },
+    },
+    {
+      files: ['**/*.+(test|stories).*'],
+      rules: {
+        'react/jsx-props-no-spreading': 'off',
       },
     },
   ],

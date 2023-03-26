b489db742aae40e80e736729d229082f5a7448ce
aprets <15954684+aprets@users.noreply.github.com>
2023-03-26 21:14:34 +0100

    update length check, prevent Is

--- index.js
+++ index.js
@@ -23,8 +23,35 @@ module.exports = {
     'prettier/prettier': 'error',
 
     /* General rules */
+    // Extend eslint-config-airbnb-typescript with type prefix rules
+    '@typescript-eslint/naming-convention': [
+      'error',
+      // Allow camelCase variables (23.2), PascalCase variables (23.8), and UPPER_CASE variables (23.10)
+      {
+        selector: 'variable',
+        format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+      },
+      // Allow camelCase functions (23.2), and PascalCase functions (23.8)
+      {
+        selector: 'function',
+        format: ['camelCase', 'PascalCase'],
+      },
+      // Airbnb recommends PascalCase for classes (23.3), and although Airbnb does not make TypeScript recommendations, we are assuming this rule would similarly apply to anything "type like", including interfaces, type aliases, and enums
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+        // Forbid starting with I as this is generally advised against in TS
+        // https://stackoverflow.com/a/41967120
+        custom: {
+          regex: '^I[A-Z]',
+          match: false,
+        },
+      },
+    ],
     // Our APIs already use null and there can be good reasons to use it
     'unicorn/no-null': 'off',
+    // !arr.length is fairly common and more concise than arr.length === 0
+    'unicorn/explicit-length-check': 'off',
     // Too restrictive, writing ugly code to defend against a very unlikely scenario: https://eslint.org/docs/rules/no-prototype-builtins
     'no-prototype-builtins': 'off',
     // https://basarat.gitbook.io/typescript/main-1/defaultisbad
